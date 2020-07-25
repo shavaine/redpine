49980d30e0627330d414dd2ce54549c77d531c0e
shavaine <35738478+shavaine@users.noreply.github.com>
2020-07-25 05:58:55 -0400

    update general amenities sec
    - Changed Icons for general amenities and added title

--- src/views/Amenities.vue
+++ src/views/Amenities.vue
@@ -8,38 +8,39 @@
     </v-row>
     <v-row>
       <v-container fluid class="footer-color text-center ">
+        <div class="text-h5 amenityText my-3">General Amenities</div>
         <v-row justify="center" class="amenityIcon">
           <v-col :cols="2"
-            ><v-icon size="48px" color="#b71c1c">mdi-phone</v-icon></v-col
+            ><v-icon size="48px" color="#b71c1c">mdi-hours-24</v-icon></v-col
           >
           <v-col :cols="2"
-            ><v-icon size="48px" color="#b71c1c">mdi-phone</v-icon></v-col
+            ><v-icon size="48px" color="#b71c1c">mdi-parking</v-icon></v-col
           >
           <v-col :cols="2"
-            ><v-icon size="48px" color="#b71c1c">mdi-phone</v-icon></v-col
+            ><v-icon size="48px" color="#b71c1c">mdi-rv-truck</v-icon></v-col
           >
           <v-col :cols="2"
-            ><v-icon size="48px" color="#b71c1c">mdi-phone</v-icon></v-col
+            ><v-icon size="48px" color="#b71c1c">mdi-washing-machine</v-icon></v-col
           >
         </v-row>
         <v-row justify="center" class="amenityText">
           <v-col :cols="2" class="pt-0">24-Hour Front Desk</v-col>
           <v-col :cols="2" class="pt-0">Free Parking</v-col>
-          <v-col :cols="2" class="pt-0">Air Conditioned</v-col>
+          <v-col :cols="2" class="pt-0">Recreational Vehicle Parking</v-col>
           <v-col :cols="2" class="pt-0">Guest Laundromat</v-col>
         </v-row>
         <v-row justify="center">
           <v-col :cols="2"
-            ><v-icon size="48px" color="#b71c1c">mdi-phone</v-icon></v-col
+            ><v-icon size="48px" color="#b71c1c">mdi-fax</v-icon></v-col
           >
           <v-col :cols="2"
-            ><v-icon size="48px" color="#b71c1c">mdi-phone</v-icon></v-col
+            ><v-icon size="48px" color="#b71c1c">mdi-spray-bottle</v-icon></v-col
           >
           <v-col :cols="2"
-            ><v-icon size="48px" color="#b71c1c">mdi-phone</v-icon></v-col
+            ><v-icon size="48px" color="#b71c1c">mdi-account-group</v-icon></v-col
           >
           <v-col :cols="2"
-            ><v-icon size="48px" color="#b71c1c">mdi-phone</v-icon></v-col
+            ><v-icon size="48px" color="#b71c1c">mdi-smoking-off</v-icon></v-col
           >
         </v-row>
         <v-row justify="center" class="amenityText">
@@ -47,6 +48,26 @@
           <v-col :cols="2" class="pt-0">Daily Housekeeping</v-col>
           <v-col :cols="2" class="pt-0">Meeting Facilities</v-col>
           <v-col :cols="2" class="pt-0">Non-Smoking Rooms</v-col>
+        </v-row>
+        <v-row justify="center" class="amenityIcon">
+          <v-col :cols="2"
+          ><v-icon size="48px" color="#b71c1c">mdi-bed</v-icon></v-col
+          >
+          <v-col :cols="2"
+          ><v-icon size="48px" color="#b71c1c">mdi-wheelchair-accessibility</v-icon></v-col
+          >
+          <v-col :cols="2"
+          ><v-icon size="48px" color="#b71c1c">mdi-snowflake</v-icon></v-col
+          >
+          <v-col :cols="2"
+          ><v-icon size="48px" color="#b71c1c">mdi-air-conditioner</v-icon></v-col
+          >
+        </v-row>
+        <v-row justify="center" class="amenityText">
+          <v-col :cols="2" class="pt-0">Rollaway Beds</v-col>
+          <v-col :cols="2" class="pt-0">Wheel Chair Access</v-col>
+          <v-col :cols="2" class="pt-0">Ice Machines</v-col>
+          <v-col :cols="2" class="pt-0">Air Conditioned</v-col>
         </v-row>
       </v-container>
     </v-row>
