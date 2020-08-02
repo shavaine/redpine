5b37c072b1db116f5e963450550d01d04401d4f8
shavaine <35738478+shavaine@users.noreply.github.com>
2020-08-02 05:08:19 -0400

    Content added for Things to do page

--- src/views/ThingsToDo.vue
+++ src/views/ThingsToDo.vue
@@ -6,12 +6,104 @@
         <div class="introText text-h6 mt-4 font-weight-black">Things To Do</div>
       </v-col>
     </v-row>
+    <v-row class="grey lighten-1 pa-3 Major-amenities" align-content="center">
+      <v-container fluid>
+        <v-row justify="center">
+          <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+            <v-card color="#363636" dark>
+              <v-card-title v-text="card.title"></v-card-title>
+              <v-card-subtitle v-text="card.subtitle"></v-card-subtitle>
+              <v-img
+                :src="card.src"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="250px"
+              >
+              </v-img>
+              <v-card-actions class="pa-0"> </v-card-actions>
+              <v-card-text v-text="card.text"></v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-col v-for="card in cards1" :key="card.title" :cols="card.flex">
+            <v-card color="#363636" dark>
+              <v-card-title v-text="card.title"></v-card-title>
+              <v-card-subtitle v-text="card.subtitle"></v-card-subtitle>
+              <v-img
+                :src="card.src"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="250px"
+              >
+              </v-img>
+              <v-card-actions class="pa-0"> </v-card-actions>
+              <v-card-text v-text="card.text"></v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "ThingsToDo"
+  name: "ThingsToDo",
+  data: () => ({
+    cards: [
+      {
+        title: "Tanger Outlet",
+        subtitle: "15km",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 3
+      },
+      {
+        title: "Museum On The Boyne",
+        subtitle: "15km",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 3
+      },
+      {
+        title: "The Gibson Centre",
+        subtitle: "15km",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 3
+      }
+    ],
+    cards1: [
+      {
+        title: "Imagine Cinemas",
+        subtitle: "15km",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+        src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
+        flex: 3
+      },
+      {
+        title: "Barrie Attractions",
+        subtitle: "15km",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 3
+      },
+      {
+        title: "GTA Attractions",
+        subtitle: "15km",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 3
+      }
+    ]
+  })
 };
 </script>
 
